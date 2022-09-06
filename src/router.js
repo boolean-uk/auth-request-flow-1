b91082b66083c2e2274288f7690723dbd0b00802
deliailu27 <54033364+deliailu27@users.noreply.github.com>
2022-09-06 23:49:43 +0100

    finished

--- src/router.js
+++ src/router.js
@@ -1,25 +1,41 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
 const mockUser = {
-    username: 'authguy',
-    password: 'mypassword',
-    profile: {
-        firstName: 'Chris',
-        lastName: 'Wolstenholme',
-        age: 43
-    }
+  username: "authguy",
+  password: "mypassword",
+  profile: {
+    firstName: "Chris",
+    lastName: "Wolstenholme",
+    age: 43,
+  },
 };
 
-router.post('/login', (req, res) => {
-
+router.post("/register", (req, res) => {
+  const { username, password } = req.body;
+  const token = jwt.sign({ id: 1, username }, password);
+  res.json(token);
 });
 
-router.get('/profile', (req, res) => {
-  
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  if (username !== "authguy" || password !== "mypassword") {
+    res.json(error, "false");
+  }
+  const token = jwt.sign({ username }, password);
+  res.json(token);
 });
 
+router.get("/profile", (req, res) => {
+  console.log(req.get("authorization"));
+  const auth = req.get("authorization");
+  try {
+    res.json(jwt.verify(auth, "mypassword"));
+  } catch (e) {
+    res.json("false");
+  }
+});
 
 module.exports = router;
